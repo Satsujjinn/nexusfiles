a02c8e5c9eb7c2f3f74431818c1da02a4ebfc5ea
Satsujjinn <leonjordaan10@gmail.com>
2025-06-05 00:27:13 +0200

    Simplify Package.swift

--- Package.swift
+++ Package.swift
@@ -1,87 +1,53 @@
 // swift-tools-version:5.9
 import PackageDescription
 
+var targets: [Target] = [
+    .target(
+        name: "NexusFiles",
+        dependencies: [
+            .product(name: "xlsxwriter", package: "xlsxwriter.swift"),
+            "CoreXLSX",
+            "SwiftCSV"
+        ],
+        path: "Sources",
+        resources: [.process("Localization")]
+    ),
+    .executableTarget(
+        name: "NexusFilesMac",
+        dependencies: ["NexusFiles"],
+        path: "MacApp"
+    ),
+    .testTarget(
+        name: "NexusFilesTests",
+        dependencies: ["NexusFiles"],
+        path: "Tests"
+    )
+]
+
 #if os(iOS)
-let package = Package(
-    name: "NexusFiles",
-    defaultLocalization: "en",
-    platforms: [
-        .iOS(.v17), .macOS(.v14)
-    ],
-    products: [
-        .library(name: "NexusFiles", targets: ["NexusFiles"]),
-        .executable(name: "NexusFilesMac", targets: ["NexusFilesMac"])
-    ],
-    dependencies: [
-        .package(url: "https://github.com/damuellen/xlsxwriter.swift", from: "1.0.0"),
-        .package(url: "https://github.com/CoreOffice/CoreXLSX.git", from: "0.9.0"),
-        .package(url: "https://github.com/swiftcsv/SwiftCSV.git", from: "0.6.0")
-    ],
-    targets: [
-        .target(
-            name: "NexusFiles",
-            dependencies: [
-                .product(name: "xlsxwriter", package: "xlsxwriter.swift"),
-                "CoreXLSX",
-                "SwiftCSV"
-            ],
-            path: "Sources",
-            resources: [.process("Localization")]
-        ),
-        .executableTarget(
-            name: "NexusFilesMac",
-            dependencies: ["NexusFiles"],
-            path: "MacApp"
-        ),
-        .target(
-            name: "NexusFilesShareExtension",
-            dependencies: ["NexusFiles"],
-            path: "ShareExtension"
-        ),
-        .testTarget(
-            name: "NexusFilesTests",
-            dependencies: ["NexusFiles"],
-            path: "Tests"
-        )
-    ]
-)
-#else
-let package = Package(
-    name: "NexusFiles",
-    defaultLocalization: "en",
-    platforms: [
-        .iOS(.v17), .macOS(.v14)
-    ],
-    products: [
-        .library(name: "NexusFiles", targets: ["NexusFiles"]),
-        .executable(name: "NexusFilesMac", targets: ["NexusFilesMac"])
-    ],
-    dependencies: [
-        .package(url: "https://github.com/damuellen/xlsxwriter.swift", from: "1.0.0"),
-        .package(url: "https://github.com/CoreOffice/CoreXLSX.git", from: "0.9.0"),
-        .package(url: "https://github.com/swiftcsv/SwiftCSV.git", from: "0.6.0")
-    ],
-    targets: [
-        .target(
-            name: "NexusFiles",
-            dependencies: [
-                .product(name: "xlsxwriter", package: "xlsxwriter.swift"),
-                "CoreXLSX",
-                "SwiftCSV"
-            ],
-            path: "Sources",
-            resources: [.process("Localization")]
-        ),
-        .executableTarget(
-            name: "NexusFilesMac",
-            dependencies: ["NexusFiles"],
-            path: "MacApp"
-        ),
-        .testTarget(
-            name: "NexusFilesTests",
-            dependencies: ["NexusFiles"],
-            path: "Tests"
-        )
-    ]
+targets.append(
+    .target(
+        name: "NexusFilesShareExtension",
+        dependencies: ["NexusFiles"],
+        path: "ShareExtension"
+    )
 )
 #endif
+
+let package = Package(
+    name: "NexusFiles",
+    defaultLocalization: "en",
+    platforms: [
+        .iOS(.v17), .macOS(.v14)
+    ],
+    products: [
+        .library(name: "NexusFiles", targets: ["NexusFiles"]),
+        .executable(name: "NexusFilesMac", targets: ["NexusFilesMac"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/damuellen/xlsxwriter.swift", from: "1.0.0"),
+        .package(url: "https://github.com/CoreOffice/CoreXLSX.git", from: "0.9.0"),
+        .package(url: "https://github.com/swiftcsv/SwiftCSV.git", from: "0.6.0")
+    ],
+    targets: targets
+)
